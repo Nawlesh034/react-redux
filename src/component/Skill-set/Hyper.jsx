0e06969aeac2a6b389530e7aeaa1c701f93c2519
Nawlesh034 <nn802301@gmail.com>
2025-02-02 22:59:42 +0530

    check 3

--- src/component/Skill-set/Hyper.jsx
+++ src/component/Skill-set/Hyper.jsx
@@ -7,16 +7,16 @@ function Hyper() {
     const dispatch = useDispatch(); 
   return (
     <div className='flex flex-col md:flex-row items-center bg-white border border-gray-300 rounded-xl shadow-md max-w-[40rem] p-6 md:p-8 gap-4 w-full'>
-    <div className='flex items-center gap-4 w-full md:w-auto justify-center md:justify-start'>
+    <div className='flex flex-col gap-4 w-full md:w-auto  md:justify-start'>
       <img src={html} alt='HTML5 Logo' className='w-16' />
-      <div className='text-center md:text-left'>
+      <div className=' md:text-left'>
         <h3 className='text-lg font-bold'>Hyper Text Markup Language</h3>
         <p className='text-gray-600 text-sm'>
           Questions: <span className='font-medium'>08</span> | Duration: <span className='font-medium'>15 mins</span> | Submitted on <span className='font-medium'>5 June 2021</span>
         </p>
       </div>
     </div>
-    <div className='w-full md:w-auto flex justify-center md:justify-end'>
+    <div className='w-full md:w-auto flex  md:justify-end'>
       <Button className=' text-white  py-2 h-fit px-6 rounded-lg font-semibold ' onClick={() => dispatch(openPopup())} style={{   backgroundColor: "rgb(30 58 138 / var(--tw-bg-opacity, 1))"
 , color:"#fff"}}>
         Update
